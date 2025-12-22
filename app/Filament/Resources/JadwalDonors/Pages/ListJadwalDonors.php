aa35fb41868de0746598a4cebae5c7fa532235c6
robbishimozuki <robbishimozuki@gmail.com>
2025-12-22 22:10:39 +0800

    update ListJadwalDonors.php

--- app/Filament/Resources/JadwalDonors/Pages/ListJadwalDonors.php
+++ app/Filament/Resources/JadwalDonors/Pages/ListJadwalDonors.php
@@ -13,7 +13,7 @@ class ListJadwalDonors extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            CreateAction::make()->visible(fn() => auth()->user()?->hasAnyRole(['admin', 'staff'])),
         ];
     }
 }
