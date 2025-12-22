b477a6d7eaac349c671bbdb6d6894463c0f30736
robbishimozuki <robbishimozuki@gmail.com>
2025-12-22 22:10:04 +0800

    update ListBloodRequests.php

--- app/Filament/Resources/BloodRequests/Pages/ListBloodRequests.php
+++ app/Filament/Resources/BloodRequests/Pages/ListBloodRequests.php
@@ -13,7 +13,7 @@ class ListBloodRequests extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            CreateAction::make()->visible(fn() => auth()->user()?->hasAnyRole(['rumah_sakit'])),
         ];
     }
 }
