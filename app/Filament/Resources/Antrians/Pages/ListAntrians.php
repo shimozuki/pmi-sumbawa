c6e3b800acb31007307b4cb35746d8ef2bc19256
robbishimozuki <robbishimozuki@gmail.com>
2025-12-28 08:44:18 +0800

    Update ListAntrians.php

--- app/Filament/Resources/Antrians/Pages/ListAntrians.php
+++ app/Filament/Resources/Antrians/Pages/ListAntrians.php
@@ -13,7 +13,7 @@ class ListAntrians extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            
         ];
     }
 }
